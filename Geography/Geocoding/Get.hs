f61d807ad8550be80394de3fbeb70cee0653d65c
Matthew Danish <mrd@debian.org>
2011-10-14 18:59:23 -0400

    import

---
+++ Geography/Geocoding/Get.hs
@@ -0,0 +1,49 @@
+-- | Some handy functions for retrieving a web-page.
+module Geography.Geocoding.Get (get, maybeGet, eitherGet, timeoutGet) where
+
+import Data.Char (intToDigit)
+import Control.Concurrent ( threadDelay, newEmptyMVar, myThreadId
+                          , forkIO, putMVar, killThread, takeMVar )
+import Network.HTTP ( simpleHTTP, insertHeaders, Header (..), HeaderName (..)
+                    , Request (..), RequestMethod (..), rspBody )
+import Network.URI (parseURI, URI)
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr)
+
+err :: String -> IO a
+err msg = do
+  hPutStrLn stderr msg
+  exitFailure
+
+get :: URI -> IO String
+get uri = do
+  eresp <- simpleHTTP $ insertHeaders [Header HdrAccept "*/*"]
+                                      (Request uri GET [] "")
+  case eresp of
+    Left er -> fail $ show er
+    Right res -> return $ rspBody res
+
+maybeGet :: URI -> IO (Maybe String)
+maybeGet uri = either (const Nothing) Just `fmap` eitherGet uri
+
+eitherGet :: URI -> IO (Either String String)
+eitherGet uri = timeoutGet uri
+
+-- 5 sec
+waitTimeout = 10000000
+
+timeoutGet :: URI -> IO (Either String String)
+timeoutGet uri = do
+  mv   <- newEmptyMVar
+  mid  <- myThreadId
+  tid1 <- forkIO $ do
+    x <- get uri 
+    putMVar mv $ Right x
+    `catch` (putMVar mv . Left . show)
+  tid2 <- forkIO $ do
+    threadDelay waitTimeout 
+    killThread tid1 
+    putMVar mv (Left "timeout")
+  takeMVar mv
+
---
+++ Geography/Geocoding/Google.hs
@@ -0,0 +1,80 @@
+-- | Google Geocoding interface. Please read
+-- <URL: http://code.google.com/apis/maps/documentation/geocoding/>
+module Geography.Geocoding.Google (geoEncode, geoDecode, GeocodeError) where
+
+import qualified Text.HJson as J
+import qualified Data.Map as M
+import Geography.Geocoding.Get
+import Network.URI (parseURI)
+import Network.HTTP (urlEncodeVars)
+import Data.Maybe (fromMaybe)
+import Text.Printf (printf)
+
+type GeocodeError = String
+
+-- | Convert an address into a latitude, longitude pair.
+geoEncode :: String -> IO (Either GeocodeError (Double, Double))
+geoEncode a = do
+  case parseURI (mkGeoEncodeURL a) of
+    Nothing  -> return (Left "URL encoding error")
+    Just uri -> do
+      jstring <- fromMaybe "" `fmap` maybeGet uri
+      case J.fromString jstring of
+        Left e -> return $ Left (show e)
+        Right js
+          | getStatus js /= "OK" -> return . Left $ getStatus js
+          | otherwise            -> return $ fromMaybe (Left "Malformed JSON") (Right `fmap` findLatLon js)
+
+-- | Convert a latitude, longitude pair into a street address.
+geoDecode :: (Double, Double) -> IO (Either GeocodeError String)
+geoDecode ll = do
+  case parseURI (mkGeoDecodeURL ll) of
+    Nothing  -> return (Left "URL encoding error")
+    Just uri -> do
+      jstring <- fromMaybe "" `fmap` maybeGet uri
+      case J.fromString jstring of
+        Left e -> return $ Left (show e)
+        Right js
+          | getStatus js /= "OK" -> return . Left $ getStatus js
+          | otherwise            -> return $ fromMaybe (Left "Malformed JSON") (Right `fmap` findAddress js)
+
+getStatus :: J.Json -> String
+getStatus (J.JObject top) = fromMaybe "Parse error" $ do
+  J.JString s <- M.lookup "status" top
+  return s
+
+findLatLon :: J.Json -> Maybe (Double, Double)
+findLatLon (J.JObject top) = do
+  J.JArray res <- M.lookup "results" top
+  searchJArray res "geometry" $ \ (J.JObject geo) -> do
+    J.JObject loc <- M.lookup "location" geo
+    J.JNumber lat <- M.lookup "lat" loc
+    J.JNumber lon <- M.lookup "lng" loc
+    return (fromRational lat, fromRational lon)
+
+findAddress :: J.Json -> Maybe String
+findAddress (J.JObject top) = do
+  J.JArray res <- M.lookup "results" top
+  searchJArray res "formatted_address" $ \ (J.JString fma) -> return fma
+
+geocodeURLFormat = "http://maps.googleapis.com/maps/api/geocode/json?%s"
+mkGeoEncodeURL :: String -> String
+mkGeoEncodeURL a =
+  printf geocodeURLFormat $ urlEncodeVars [("address", a), ("sensor", "false")]
+mkGeoDecodeURL :: (Double, Double) -> String
+mkGeoDecodeURL (lat, lng) =
+  printf geocodeURLFormat $ urlEncodeVars [("latlng", ll), ("sensor", "false")]
+  where ll = show lat ++ "," ++ show lng
+
+searchJArray a name f =
+  flip findJust a $ \ x ->
+    case x of J.JObject m -> do
+                y <- M.lookup name m
+                f y
+              _ -> Nothing
+
+findJust :: (a -> Maybe b) -> [a] -> Maybe b
+findJust f [] = Nothing
+findJust f (x:xs) = case f x of
+   (y@ (Just _)) -> y
+   Nothing -> findJust f xs
